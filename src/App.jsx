58ad930c807490287d3370af0abb7c21f77df1db
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-01-11 09:59:02 +0100

    {children}

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 import FixedContainer from './components/LayoutContainer/FixedContainer.jsx'
+import FullWidthGrid from './components/LayoutGrid/FullWidthGrid.jsx'
 
 function App() {
   return (
       <FixedContainer>
-    
+      <FullWidthGrid>
+      </FullWidthGrid>
       </FixedContainer>
   );
 }
--- src/components/LayoutContainer/FixedContainer.jsx
+++ src/components/LayoutContainer/FixedContainer.jsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Box, Container } from '@mui/system';
 
-export default function FixedContainer() {
+export default function FixedContainer( {children} ) {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container fixed>
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+        <Box sx={{ bgcolor: '#fff', height: '100vh' }} />
       </Container>
     </React.Fragment>
   );
