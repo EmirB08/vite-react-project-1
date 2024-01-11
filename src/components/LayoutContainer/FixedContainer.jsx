209a6f65575bb58fe67440ad5a0ea5a3253733fb
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-01-11 10:06:33 +0100

    prop types and passing children parameter fix for FixedContainer

--- src/components/LayoutContainer/FixedContainer.jsx
+++ src/components/LayoutContainer/FixedContainer.jsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes
 import CssBaseline from '@mui/material/CssBaseline';
 import { Box, Container } from '@mui/system';
 
-export default function FixedContainer( {children} ) {
+const FixedContainer = ({ children }) => {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container fixed>
-        <Box sx={{ bgcolor: '#fff', height: '100vh' }} />
+        <Box sx={{ bgcolor: '#fff', height: '100vh' }}>
+          {children}
+        </Box>
       </Container>
     </React.Fragment>
   );
-}
+};
+
+FixedContainer.propTypes = {
+  children: PropTypes.node
+};
+
+export default FixedContainer;
