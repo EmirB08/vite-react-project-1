e75d3995b26afdc5faeb4646d1853248feab0085
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-01-17 15:28:03 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { posts } from './data/posts';
 import FixedContainer from './components/LayoutContainer/FixedContainer.jsx';
 import FullWidthGrid from './components/LayoutGrid/FullWidthGrid.jsx';
 import HeaderNavBar from './components/HeaderNavBar/HeaderNavBar.jsx';
+import Footer from './components/Footer/Footer';
 
 function App() {
   console.log('Posts object:', posts);
@@ -11,6 +12,7 @@ function App() {
     <FixedContainer>
     <HeaderNavBar />
     <FullWidthGrid articles={posts.articles} />
+    <Footer />
     </FixedContainer>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return ( 
+        <footer className="footer">
+        <p>Made with React + Vite</p>
+        <a href="https://github.com/EmirB08" target="_blank" rel="noreferrer">
+        @Github
+        </a>
+        </footer>
+    );  
+}
