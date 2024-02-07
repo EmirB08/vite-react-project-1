ce2916084722fa316da6e8a0e87aa0978566e323
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-02-07 13:49:15 +0100

    Footer.jsx updated + formatting with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import './App.css';
-import { posts } from './data/posts';
-import FixedContainer from './components/LayoutContainer/FixedContainer.jsx';
-import FullWidthGrid from './components/LayoutGrid/FullWidthGrid.jsx';
-import HeaderNavBar from './components/HeaderNavBar/HeaderNavBar.jsx';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import { posts } from "./data/posts";
+import FixedContainer from "./components/LayoutContainer/FixedContainer.jsx";
+import FullWidthGrid from "./components/LayoutGrid/FullWidthGrid.jsx";
+import HeaderNavBar from "./components/HeaderNavBar/HeaderNavBar.jsx";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  console.log('Posts object:', posts);
-  return (
-    <FixedContainer>
-    <HeaderNavBar />
-    <FullWidthGrid articles={posts.articles} />
-    <Footer />
-    </FixedContainer>
-  );
+	return (
+		<FixedContainer>
+			<HeaderNavBar />
+			<FullWidthGrid articles={posts.articles} />
+			<Footer />
+		</FixedContainer>
+	);
 }
 
 export default App;
-
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,60 @@
-import "./Footer.css";
+import ReactIcon from "../../media/react-icon.svg";
+import ViteIcon from "../../media/vite-icon.svg";
+import GitHubIcon from "../../media/github-icon.svg";
+import {
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton,
+	Container,
+} from "@mui/material";
 
-export default function Footer() {
-    return ( 
-        <footer className="footer">
-        <p>Made with React + Vite by</p>
-        <a href="https://github.com/EmirB08" target="_blank" rel="noreferrer">
-        Emir@GitHub
-        </a>
-        </footer>
-    );  
-}
+const Footer = () => (
+	<Container maxWidth="md">
+		<AppBar
+			position="static"
+			sx={{ backgroundColor: "#f8f8f8d8", boxShadow: "none", border: 0 }}>
+			<Toolbar sx={{ minHeight: "45px !important", padding: "0px" }}>
+				<Typography
+					variant="h6"
+					color="primary"
+					component="div"
+					sx={{
+						flexGrow: 1,
+						fontSize: "0.9rem",
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+						fontWeight: "bold",
+					}}>
+					Created with
+					<IconButton
+						color="primary"
+						href="https://vitejs.dev"
+						target="_blank"
+						rel="noopener noreferrer">
+						<img src={ViteIcon} alt="Vite" style={{ height: 24 }} />
+					</IconButton>{" "}
+					+
+					<IconButton
+						color="primary"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer">
+						<img src={ReactIcon} alt="React" style={{ height: 24 }} />
+					</IconButton>{" "}
+					by EmirB08
+					<IconButton
+						color="primary"
+						href="https://github.com/EmirB08"
+						target="_blank"
+						rel="noopener noreferrer">
+						<img src={GitHubIcon} alt="GitHub" style={{ height: 24 }} />
+					</IconButton>
+				</Typography>
+			</Toolbar>
+		</AppBar>
+	</Container>
+);
+
+export default Footer;
